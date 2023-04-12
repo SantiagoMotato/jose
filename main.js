c631d3dacf33ce4e470191a425480c3591af857a
YipGiH <yenigomez2205@gmail.com>
2023-04-12 14:31:05 -0500

    Ficha tecnica

--- main.js
+++ main.js
@@ -3,21 +3,13 @@ function verFicha(){
    let nomp,lineap,cantidad,valor,vt,facturaVenta,cuentaCobro,lectorCodigo,inventario
  
    nomp = document.getElementById("nombre").value
-
    lineap = document.getElementById("linea").value
-
    cantidad = document.getElementById("cantidad").value
-
    valor =  document.getElementById("valor").value
-
    facturaVenta = document.getElementById("impresora").value
-
    vt = cantidad * valor
-
    cuentaCobro = document.getElementById("cobro").value
-
    lectorCodigo = document.getElementById("lector").value
-
    inventario = document.getElementById("inventario").value
 
 
@@ -30,6 +22,5 @@ document.getElementById("marcaImpresora").innerHTML = "<h2>Nombre de la impresor
 document.getElementById("cuentCobro").innerHTML = "<h2>Cuenta de cobro: "+cuentaCobro+"</h2>"
 document.getElementById("lectorCodigo").innerHTML = "<h2>Lector de codigo: "+lectorCodigo+"</h2>"
 document.getElementById("inventarioPro").innerHTML = "<h2>Nombre del computador/es: "+inventario+"</h2>"
-
 
 }
